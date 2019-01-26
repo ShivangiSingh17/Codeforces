761f59cf1ecd510a8d407a98ed018e61d927b47e
Shivangi Singh <shivangisingh1702@gmail.com>
2019-01-27 01:01:59 +0530

    Problem/1075/A     TheKingRace
    Problem/1075/A     TheKingRace

---
+++ TheKingRace.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	long long int a,b,c,s,s1;
+	scanf("%I64d""%I64d""%I64d",&a,&b,&c);
+	s=b+c;
+	s1=a+2;
+	if(s1>s)
+	printf("white");
+	else
+	printf("black");
+	return 0;
+}
+
